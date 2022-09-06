57259cf9dc57609c5c23c37e61e684488fccaca8
julius-ahmad <julius.ahmad02@gmail.com>
2022-09-06 19:25:11 +0100

    updated with current functions

---
+++ main.py
@@ -0,0 +1,9 @@
+import history_handler
+
+if __name__ == '__main__':
+
+    username = 'pastaman4'
+    hand_history = '/Users/juliusahmad/Library/Application Support/PokerStarsUK/HandHistory/pastaman4'
+
+    sessions = history_handler.read_history(hand_history)
+    history_handler.clean_up(sessions, username)
