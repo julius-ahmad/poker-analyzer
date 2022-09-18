d70ec5ce72d9456195c20ea673565da0e2c087b9
julius-ahmad <julius.ahmad02@gmail.com>
2022-09-18 22:47:36 +0100

    removed duplicate wins_losses

--- history_handler.py
+++ history_handler.py
@@ -1,6 +1,5 @@
 import glob
 import os
-MAX_PLAYERS = 9
 
 
 def read_history(pathname):
@@ -36,37 +35,4 @@ def summarizer(sessions):
                 games['cards'].append(ses[i + 1][-8:-1])
 
     return games
-
-
-def wins_losses(sessions, username):
-    """
-    For now, I am just looking at game summaries.
-    Seat 1 is always button for zoom only. Can calculate position from there
-    :return:
-    """
-    games = summarizer(sessions)
-
-    positions = {'button': {'won': 0, 'lost': 0}, 'small blind': {'won': 0, 'lost': 0}, 'big blind':
-        {'won': 0, 'lost': 0}, 'UTG': {'won': 0, 'lost': 0}, 'UTG+1': {'won': 0, 'lost': 0}, 'MP': {'won': 0, 'lost': 0}
-        , 'MP+1': {'won': 0, 'lost': 0}, 'HJ': {'won': 0, 'lost': 0}, 'CO': {'won': 0, 'lost': 0}}
-
-    for summary in games['summary']:
-        players = int(summary[-1][5])
-        for line in summary:
-            if 'button' in line:
-                button_seat = int(line[5])
-        adjusted = {str((button_seat + i - 1) % players + 1): list(positions.keys())[i] for i in range(players)}
-        if players < MAX_PLAYERS:
-            s = (button_seat + 2) % players
-            for pos in list(positions.keys())[-1 * (players-3):]:
-                adjusted[str(s % players + 1)] = pos
-                s += 1
-        for line in summary:
-            if username in line:
-                if 'collected' in line:
-                    positions[adjusted[line[5]]]['won'] += 1
-                else:
-                    positions[adjusted[line[5]]]['lost'] += 1
-
-    print(positions)
 
--- main.py
+++ main.py
@@ -6,7 +6,5 @@ if __name__ == '__main__':
     username = 'pastaman4'
     hand_history = '/Users/juliusahmad/Library/Application Support/PokerStarsUK/HandHistory/pastaman4'
 
-    sessions = history_handler.read_history(hand_history)
-    print(history_handler.wins_losses(sessions, username))
     analyzer = analyzer.Analyzer(username, hand_history)
     print(analyzer.wins_losses())
